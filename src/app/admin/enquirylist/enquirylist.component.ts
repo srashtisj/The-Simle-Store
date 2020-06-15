9b76f4fd24ecebdefcef7d04c830f976364827ff
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-06-15 15:22:28 +0530

    Updates

--- src/app/admin/enquirylist/enquirylist.component.ts
+++ src/app/admin/enquirylist/enquirylist.component.ts
@@ -1,7 +1,4 @@
-import { EnquriyService } from './../../service/enquiry/enquriy.service';
-import { Enquiry } from './../../interface/enquiry/enquiry';
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-enquirylist',
@@ -9,22 +6,7 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./enquirylist.component.scss'],
 })
 export class EnquirylistComponent implements OnInit {
-  displayedColumns = ['name', 'email', 'mobile', 'Description'];
-  datasource = new MatTableDataSource<Enquiry>();
+  constructor() {}
 
-
-
-
-  constructor(private enquiry: EnquriyService) {}
-
-  ngOnInit(): void {
-
-    this.enquiry.enquirydata.subscribe((re:Enquiry[])=>{
-      this.datasource.data=re;
-    })
-
-
-
-
-  }
+  ngOnInit(): void {}
 }
--- src/app/service/enquiry/enquriy.service.ts
+++ src/app/service/enquiry/enquriy.service.ts
@@ -1,7 +1,7 @@
 import { Enquiry } from './../../interface/enquiry/enquiry';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { map } from 'rxjs/operators';
+
 import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
@@ -9,23 +9,18 @@ import { Subject } from 'rxjs';
 export class EnquriyService {
   constructor(private db: AngularFirestore) {}
 
-  enquirydata=new Subject<Enquiry[]>();
+  Enquirydata= new Subject<Enquiry[]>();
 
-  Addenquiry(enquiry:Enquiry){
-    this.db.collection('enquiry').add(enquiry).then((re)=>{
-      console.log('success');
-
-    }).catch((error)=>{
-      console.log(error.message);
-
-    });
+  Addenquiry(enquiry: Enquiry) {
+    this.db
+      .collection('enquiry')
+      .add(enquiry)
+      .then((re) => {
+        console.log('success');
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
-  getenquiry(){
-   this.db
-     .collection('enquiry')
-     .valueChanges()
-     .subscribe((re: Enquiry[]) => {
-       this.enquirydata.next(re);
-     });
-  }
+
 }
